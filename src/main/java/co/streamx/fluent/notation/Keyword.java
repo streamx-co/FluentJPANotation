bc5e4b3b02bb2733240116568b71dd00546e5679
Konstantin Triger <kostat@gmail.com>
2019-09-20 00:30:43 +0300

    TableExtension

--- src/main/java/co/streamx/fluent/notation/Keyword.java
+++ src/main/java/co/streamx/fluent/notation/Keyword.java
@@ -1,12 +1,12 @@
 package co.streamx.fluent.notation;
 
-import java.util.Arrays;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public interface Keyword {
     @Local
     static Keyword join(Keyword... keywords) {
-        String joined = Arrays.stream(keywords).map(Object::toString).collect(Collectors.joining());
+        String joined = Stream.of(keywords).map(Object::toString).collect(Collectors.joining());
         return new Keyword() {
             @Override
             public String toString() {
